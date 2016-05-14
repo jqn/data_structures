e946fb51a8b8436dab2e1c8403a274ff119489be
Jorge Téllez <novohispano@gmail.com>
2016-05-14 14:58:46 -0600

    Improve the performance of equilibrium index exercise to O(N)

--- exercises/equilibrium_index/equilibrium_index.rb
+++ exercises/equilibrium_index/equilibrium_index.rb
@@ -1,17 +1,16 @@
 class EquilibriumIndex
   def solution(a)
     n = a.length - 1
+    
+    first_half  = 0
+    second_half = a.reduce(0, :+)
 
     for index in 0..n
-      first_half  = a[0..index]
-      second_half = a[(index + 1)..-1]
+      second_half -= a[index]
 
-      first_half.delete_at(index)
+      result = index if second_half == first_half
 
-      first_half_sum  = first_half.reduce(0, :+)
-      second_half_sum = second_half.reduce(0, :+)
-
-      result = index if second_half_sum == first_half_sum
+      first_half += a[index]
     end
 
     result ? result : -1
